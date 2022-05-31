3a26cae1a4914585f2000fa2e9d4a6277a2a90ad
kunxin-chor <chorkunxin@yahoo.com>
2022-05-31 07:38:36 +0000

    Added display search results and markers

--- 06-foursquare-api/script.js
+++ 06-foursquare-api/script.js
@@ -1,28 +1,44 @@
-// L.map will create a new map object
-// the first argument is the ID of the
-// element that will have the
-let map = L.map('map');
 
-// set the center of the map
-// has 2 arguments
-// first argument: the center point 
-// second argument: the higher it is
-// the more zoomed in the map will be
-map.setView([1.3521, 103.8198], 13);
 
-// add the tile layer for the map
-// first argument: data source of your map
-// second argument: attribution text
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',  // style of the tiles
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
-}).addTo(map);
+window.addEventListener('DOMContentLoaded', async function () {
+    let map = createMap(1.3521, 103.8198);
+    let searchResultLayer = L.layerGroup();
+    searchResultLayer.addTo(map)
 
-window.addEventListener('DOMContentLoaded', async function(){
-    let results = await search(1.3521, 103.8198, "Cafe", 10000);
-    console.log(results);
+    document.querySelector('#btnSearch').addEventListener('click', async function () {
+
+        // clear existing markers from the search result layer
+        searchResultLayer.clearLayers(); //<-- will delete all layers inside it
+
+        // clear existing search results
+        document.querySelector('#results').innerHTML = "";
+
+        let query = document.querySelector('#txtSearch').value;
+        let latlng = map.getBounds().getCenter();
+        let locations = await search(latlng.lat, latlng.lng, query, 4000);
+        for (let result of locations.results) {
+
+            // create markers and put on map
+            let lat = result.geocodes.main.latitude;
+            let lng = result.geocodes.main.longitude;
+
+            let marker = L.marker([lat, lng]).addTo(searchResultLayer);
+
+            marker.bindPopup(`<h1>${result.name}</h1>
+           <p>${result.location.address} 
+           ${result.location.address_extended ? ", " + result.location.address_extended
+                    : ""}</p>`)
+
+            // create the search result entry and display under the search
+            let resultElement = document.createElement('div');
+            resultElement.className="search-result";
+            resultElement.innerHTML = result.name;
+            resultElement.addEventListener('click', function(){
+                map.flyTo([lat, lng], 16)
+                marker.openPopup();
+            })
+
+            document.querySelector("#results").appendChild(resultElement);
+        }
+    })
 })
